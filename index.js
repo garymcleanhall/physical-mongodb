890ecd2b2ffaa1a1067e39b41a76270787b19038
Gary McLean Hall <gary.hall@fourminutemile.co.uk>
2017-05-08 09:08:32 +0100

    Simple connection check for mongo db

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const
+  mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+function _checkMongo(connectionString) {
+  return new Promise((resolve, reject) => {
+    let connection = mongoose.createConnection(connectionString)
+    connection.on('connected', () => {
+      connection.close()
+      resolve({ isOk: true })
+    })
+    connection.on('error', error => {
+      reject(error)
+    })
+  }).catch(error => ({ isOk: false, error}))
+}
+
+module.exports = {
+  mongodb: {
+    check: _checkMongo
+  }
+}
---
+++ tests/mongo-spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const
+  physical = require('../index')
+
+function testAsync(runAsync) {
+  return done => {
+    runAsync()
+      .then(done, error => {
+        fail(error)
+        done()
+      })
+  }
+}
+
+describe('Physical MongoDB', () => {
+
+  it('is not ok when cannot connect to mongo db', testAsync(async () => {
+    let connectionString = "mongodb://localhost:6666/_test"
+    let mongoResult = await physical.mongodb.check(connectionString)
+    expect(mongoResult.isOk).toBe(false)
+  }))
+
+  it('includes error message when cannot connect to mongo db', testAsync(async () => {
+    let connectionString = "mongodb://localhost:6666/_test"
+    let mongoResult = await physical.mongodb.check(connectionString)
+    expect(mongoResult.error).toBeDefined()
+  }))
+
+  it('is ok when can connect to mongo db', testAsync(async () => {
+    let connectionString = "mongodb://localhost:27017/_test"
+    let mongoResult = await physical.mongodb.check(connectionString)
+    expect(mongoResult.isOk).toBe(true)
+  }))
+
+})
